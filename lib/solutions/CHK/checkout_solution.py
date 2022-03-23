b3834830fea79727fe82686ba8ac5b1560f37ab1
sbx_user1051 <sbx_user1051@169.254.12.141>
2022-03-23 17:07:48 +0000

    Wed Mar 23 17:07:48 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,7 +7,8 @@ def update_items(item_counts, item1, item2, required_amount):
     """remove any items the customer is getting for free from the items_count dictionary"""
     num_free_item1s = item_counts[item2]//required_amount
     return item_counts[item1] - num_free_item1s if num_free_item1s < item_counts[item1] else 0
-    
+def group_discount_offer(item_counts):
+
 def get_total_cost(item_counts):
     price_list={A:50, B:30, C:20, D:15, E:40, F:10, G:20, H:10, I:35, J:60, K:70, L:90, M:15, N:40, O:10, P:50, Q:30, R:50, S:20, T:20, U:40, V:50, W:20, X:17, Y:20, Z:21}
     list1=["B", "M", "Q"]
