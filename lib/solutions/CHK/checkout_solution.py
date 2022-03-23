0791a2578047668be009a5536b2a5a7e761b12a7
sbx_user1051 <sbx_user1051@169.254.12.141>
2022-03-23 11:59:59 +0000

    Wed Mar 23 11:59:59 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    Input: receives a string representing the items 
+    Output: need to return the price of the items, if illegal input need to output -1
+    Note: haven't been told the exact form of the input i.e are there spaces etc?
+    also havn't been told if an empty string is an illegal input or not?
+    """
+    total=0
+    for item in skus:
+        if item==
+
