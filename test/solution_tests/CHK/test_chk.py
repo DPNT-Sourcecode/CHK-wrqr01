790452b38b16fff2fe18e5f6b90db2d7477fd417
sbx_user1051 <sbx_user1051@169.254.12.141>
2022-03-23 16:46:37 +0000

    Wed Mar 23 16:46:37 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,38 +1,39 @@
 from solutions.CHK import checkout_solution
 
 class TestChk():
-    def test_chk(self):
+    def test_chk1(self):
         assert checkout_solution.checkout("UUUUUUUU") == 240
-    # def test_chk(self):
-    #     assert checkout_solution.checkout("AAAAAAAAB") == 360
+    def test_chk2(self):
+        assert checkout_solution.checkout("EE")==80
+    def test_chk3(self):
+        assert checkout_solution.checkout("AAAAAAAAB") == 360
 
-    # def test_chk(self):
-    #     assert checkout_solution.checkout("AAAAAAAABB") == 375
+    def test_chk4(self):
+        assert checkout_solution.checkout("AAAAAAAABB") == 375
     
-    # def test_chk(self):
-    #     assert checkout_solution.checkout("EEB") == 80
+    def test_chk5(self):
+        assert checkout_solution.checkout("EEB") == 80
     
-    # def test_chk(self):
-    #     assert checkout_solution.checkout("EEEEBB") == 160
+    def test_chk6(self):
+        assert checkout_solution.checkout("EEEEBB") == 160
 
-    # def test_chk(self):
-    #     assert checkout_solution.checkout("O") == 10
+    def test_chk7(self):
+        assert checkout_solution.checkout("O") == 10
     
-    # def test_chk(self):
-    #     assert checkout_solution.checkout("RRQR") == 150
+    def test_chk8(self):
+        assert checkout_solution.checkout("RRQR") == 150
     
-    # def test_chk(self):
-    #     assert checkout_solution.checkout("F") == 10
+    def test_chk9(self):
+        assert checkout_solution.checkout("F") == 10
 
-    # def test_chk(self):
-    #     assert checkout_solution.checkout("UUU") == 120
+    def test_chk10(self):
+        assert checkout_solution.checkout("UUU") == 120
     
-    # def test_chk(self):
-    #     assert checkout_solution.checkout("UUUU") == 120
+    def test_chk11(self):
+        assert checkout_solution.checkout("UUUU") == 120
     
-    
-    
-    # def test_chk(self):
-    #     assert checkout_solution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZ") == 965
+    def test_chk12(self):
+        assert checkout_solution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZ") == 965
     
     
+
