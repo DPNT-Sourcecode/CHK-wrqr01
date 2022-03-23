acfdd0b4e4ec4d4a1268aa55d814b562a647e683
sbx_user1051 <sbx_user1051@169.254.12.141>
2022-03-23 17:10:48 +0000

    Wed Mar 23 17:10:48 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,88 +3,95 @@
 from collections import Counter
 import string
 
-def update_items(item_counts, item1, item2, required_amount):
-    """remove any items the customer is getting for free from the items_count dictionary"""
-    num_free_item1s = item_counts[item2]//required_amount
-    return item_counts[item1] - num_free_item1s if num_free_item1s < item_counts[item1] else 0
-def group_discount_offer(item_counts):
+class checkout():
 
-def get_total_cost(item_counts):
-    price_list={A:50, B:30, C:20, D:15, E:40, F:10, G:20, H:10, I:35, J:60, K:70, L:90, M:15, N:40, O:10, P:50, Q:30, R:50, S:20, T:20, U:40, V:50, W:20, X:17, Y:20, Z:21}
-    list1=["B", "M", "Q"]
-    list2=["E", "N", "R"]
-    required_amounts=[2, 3, 3]
-    for item1, item2, required_amount in zip(list1, list2, required_amounts):
-        item_counts[item1]=update_items(item_counts, item1, item2, required_amount)
+    def update_items(self, item_counts, item1, item2, required_amount):
+        """remove any items the customer is getting for free from the items_count dictionary"""
+        num_free_item1s = item_counts[item2]//required_amount
+        return item_counts[item1] - num_free_item1s if num_free_item1s < item_counts[item1] else 0
+    def group_discount_offer(self, item_counts):
+        num=0
+        for item in ["S","T","X","Y","Z"]:
+            num+=item_counts[item]
+        if num > 3:
 
-    total_cost=0
-    print(f"the number of Es: {item_counts}")
+    def get_total_cost(self, item_counts):
+        price_list={A:50, B:30, C:20, D:15, E:40, F:10, G:20, H:10, I:35, J:60, K:70, L:90, M:15, N:40, O:10, P:50, Q:30, R:50, S:20, T:20, U:40, V:50, W:20, X:17, Y:20, Z:21}
+        list1=["B", "M", "Q"]
+        list2=["E", "N", "R"]
+        required_amounts=[2, 3, 3]
+        for item1, item2, required_amount in zip(list1, list2, required_amounts):
+            item_counts[item1]=update_items(item_counts, item1, item2, required_amount)
 
-    for item in item_counts.keys():
-        if item=="A":
-            num_5s=item_counts[item]//5
-            num_3s=(item_counts[item]%5)//3
-            num_1s=item_counts[item] - (num_5s*5 + num_3s*3)
-            total_cost+= num_5s * 200 + num_3s * 130 + num_1s * price_list[item]
+        total_cost=0
+        print(f"the number of Es: {item_counts}")
 
-        elif item=="B":
-            num_2s=item_counts[item]//2
-            num_1s=item_counts[item]%2
-            total_cost+= num_2s * 45 + num_1s * price_list[item]
-        
-        elif item=="F":
-            total_cost+=item_counts[item]*price_list[item] - (item_counts[item]//3)*price_list[item]
+        for item in item_counts.keys():
+            if item=="A":
+                num_5s=item_counts[item]//5
+                num_3s=(item_counts[item]%5)//3
+                num_1s=item_counts[item] - (num_5s*5 + num_3s*3)
+                total_cost+= num_5s * 200 + num_3s * 130 + num_1s * price_list[item]
 
-        elif item=="H":
-            num_10s=item_counts[item]//10
-            num_5s=(item_counts[item]%10)//5
-            num_1s=item_counts[item] - (num_10s*10 + num_5s*5)
-            total_cost+= num_10s * 80 + num_5s * 45 + num_1s * price_list[item]
-
-        elif item=="K":
-            num_2s=item_counts[item]//2
-            num_1s=item_counts[item]%2
-            total_cost+= num_2s * 120 + num_1s * price_list[item]
-
-        elif item=="P":
-            num_5s=item_counts[item]//5
-            num_1s=item_counts[item]%5
-            total_cost+= num_5s * 200 + num_1s * price_list[item]
-        
-        elif item=="Q":
-            num_3s=item_counts[item]//3
-            num_1s=item_counts[item]%3
-            total_cost+= num_3s * 80 + num_1s * price_list[item]
-        
-        elif item=="U":
-            total_cost+=item_counts[item]*price_list[item] - (item_counts[item]//4)*price_list[item]
+            elif item=="B":
+                num_2s=item_counts[item]//2
+                num_1s=item_counts[item]%2
+                total_cost+= num_2s * 45 + num_1s * price_list[item]
             
-        elif item=="V":
-            num_3s=item_counts[item]//3
-            num_2s=(item_counts[item]%3)//2
-            num_1s=item_counts[item] - (num_3s*3 + num_2s*2)
-            total_cost+= num_3s * 130 + num_2s * 90 + num_1s * price_list[item]
-        else:
-            total_cost+=item_counts[item] * price_list[item]
+            elif item=="F":
+                total_cost+=item_counts[item]*price_list[item] - (item_counts[item]//3)*price_list[item]
 
-    return total_cost
+            elif item=="H":
+                num_10s=item_counts[item]//10
+                num_5s=(item_counts[item]%10)//5
+                num_1s=item_counts[item] - (num_10s*10 + num_5s*5)
+                total_cost+= num_10s * 80 + num_5s * 45 + num_1s * price_list[item]
+
+            elif item=="K":
+                num_2s=item_counts[item]//2
+                num_1s=item_counts[item]%2
+                total_cost+= num_2s * 120 + num_1s * price_list[item]
+
+            elif item=="P":
+                num_5s=item_counts[item]//5
+                num_1s=item_counts[item]%5
+                total_cost+= num_5s * 200 + num_1s * price_list[item]
+            
+            elif item=="Q":
+                num_3s=item_counts[item]//3
+                num_1s=item_counts[item]%3
+                total_cost+= num_3s * 80 + num_1s * price_list[item]
+            
+            elif item=="U":
+                total_cost+=item_counts[item]*price_list[item] - (item_counts[item]//4)*price_list[item]
+                
+            elif item=="V":
+                num_3s=item_counts[item]//3
+                num_2s=(item_counts[item]%3)//2
+                num_1s=item_counts[item] - (num_3s*3 + num_2s*2)
+                total_cost+= num_3s * 130 + num_2s * 90 + num_1s * price_list[item]
+            else:
+                total_cost+=item_counts[item] * price_list[item]
+
+        return total_cost
 
 
-def checkout(skus):
-    """
-    Input: receives a string representing the items 
-    Output: need to return the price of the items, if illegal input need to output -1
-    Note: haven't been told the exact form of the input i.e are there spaces etc?
-    also havn't been told if an empty string is an illegal input or not?
-    also has't been mentioned if the multi items will always come in together as nX or not.
-    """
-    total=0
-    allowed=set(string.ascii_uppercase)
-    for char in skus:
-        if char not in allowed:
-            return -1
+    def checkout(self, skus):
+        """
+        Input: receives a string representing the items 
+        Output: need to return the price of the items, if illegal input need to output -1
+        Note: haven't been told the exact form of the input i.e are there spaces etc?
+        also havn't been told if an empty string is an illegal input or not?
+        also has't been mentioned if the multi items will always come in together as nX or not.
+        """
+        total=0
+        allowed=set(string.ascii_uppercase)
+        for char in skus:
+            if char not in allowed:
+                return -1
 
-    item_counts=Counter(skus)
-    total=get_total_cost(item_counts)
+        item_counts=Counter(skus)
+        total=get_total_cost(item_counts)
 
-    return total
+        return total
+
